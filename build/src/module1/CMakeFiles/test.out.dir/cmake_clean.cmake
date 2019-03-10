cbbf71ced0f44e03fe01f6035571e64f166fa64b
Shinji Kirino <shinjikirino@gmail.com>
2019-03-10 06:14:55 +0000

    added src/module dir

---
+++ build/src/module1/CMakeFiles/test.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.out.dir/test/test.cpp.o"
+  "test.out.pdb"
+  "test.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
